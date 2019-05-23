18c479708abc615c01c3be8c13130f18b074f957
kpatt713 <kpatt713@gmail.com>
2019-05-23 13:05:18 -0500

    mostly working dashboard todo

--- myPortfolio/scripts/dashboard2.js
+++ myPortfolio/scripts/dashboard2.js
@@ -1,9 +1,25 @@
-var multiple = new Multiple({
-    selector: '.item',
-    background: 'linear-gradient(#273463, #8B4256)'
+$(function() {
+
+    let $list = $('ul');
+    let $newItemForm = $('#newItemForm');
+  
+    $newItemForm.on('submit', function(e) {
+        e.preventDefault();
+      let text = $('input[type="text"]').val();
+      $list.append(`<li>${text}</li>`);
+      $('input[type="text"]').val('');
+    });
+  
+    $list.on('click', 'li', function() {
+      let $this = $(this);
+      $this.remove();
+    });
+  
   });
 
-  function myLocation() {
+
+
+function myLocation() {
     var loc= document.getElementById("myLoc");
     if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition(showPosition);
@@ -15,3 +31,11 @@ function showPosition (position) {
     var loc= document.getElementById("myLoc");
     loc.innerHTML = "Longitude: " + position.coords.longitude + "<br>Latitude: " + position.coords.latitude + "<br>";
 }
+
+
+
+
+  var multiple = new Multiple({
+    selector: '.item',
+    background: 'linear-gradient(#273463, #8B4256)'
+  });
